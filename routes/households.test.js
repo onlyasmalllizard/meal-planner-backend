902741173e2a8c04f6c92f3c6683bd1f13d0a162
Lizard Morrow <lizardmorrow@gmail.com>
2021-12-09 10:47:32 +0000

    Create Unit Test framework for Households

---
+++ routes/households.test.js
@@ -0,0 +1,95 @@
+describe('Unit Tests: households/:id', () => {
+  test('Upon a successful GET request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful POST request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PATCH request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PUT request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful DELETE request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to server error, the correct response is returned', async () => {});
+});
+
+describe('Unit Tests: households/name', () => {
+  test('Upon a successful GET request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful POST request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PATCH request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PUT request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful DELETE request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to server error, the correct response is returned', async () => {});
+});
+
+describe('Unit Tests: households/:id/occupants', () => {
+  test('Upon a successful GET request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful GET request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful POST request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful POST request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PATCH request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PATCH request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful PUT request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful PUT request due to server error, the correct response is returned', async () => {});
+
+  test('Upon a successful DELETE request, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to user error, the correct response is returned', async () => {});
+
+  test('Upon an unsuccessful DELETE request due to server error, the correct response is returned', async () => {});
+});
